b5be0cb2169972c8f43ac35046d477b1796271af
Shivam Baranwal <59394167+shivam-baranwal@users.noreply.github.com>
2021-05-27 00:28:25 +0530

    Delete edit_bloodGroup.php

--- admin/assets/edit_bloodGroup.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('connection.php');
-
-	$id = $_GET['blood_id'];
-
-	$bname = $_POST['bname'];
-	$update = $connection->query("UPDATE blood_group SET blood_group='$bname' WHERE blood_id='$id'");
-	header('location:blood_group.php');
-?>
