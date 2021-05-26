8470835544b2e815361daad9e5a81c8d3320be79
Shivam Baranwal <59394167+shivam-baranwal@users.noreply.github.com>
2021-05-27 00:30:08 +0530

    Delete delete_members.php

--- admin/assets/delete_members.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('connection.php');
-
-	$id = $_GET['member_id'];
-
-	$delete = $connection->query("DELETE FROM member WHERE member_id='$id'");
-
-	header('location:members.php');
-?>
